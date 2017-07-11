3058ef5749a22facd12a90d75a9f0b7606c085c6
Tuterangi Pleydell <tuterangi@gmail.com>
2017-07-11 16:43:00 +1200

    hamburger menu that toggles slideout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-   
+   $(".slideoutbtn").click(function() {
+   	slideout.toggle();
+   });
 })
 
-window.onload = function() {
-	document.querySelector('.slideoutBtn').addEventListener('click', function() {
-		slideout.toggle();
-	});
-}
+// window.onload = function() {
+// 	document.querySelector('.slideoutBtn').addEventListener('click', function() {
+// 		slideout.toggle();
+// 	});
+// }
 
